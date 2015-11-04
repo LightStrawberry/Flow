0799ee42d7767c010bccebc4bc6b175faad7e3e1
LightStrawberry <l21828604@gmail.com>
2015-11-04 12:21:11 +0800

    pause

--- app/src/main/java/com/shokill/download/MainActivity.java
+++ app/src/main/java/com/shokill/download/MainActivity.java
@@ -12,9 +12,11 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
@@ -36,40 +38,43 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONObject;
 
 public class MainActivity extends Activity {
-	private static final int PROCESSING = 1;
-	private static final int FAILURE = -1;
+    private static final int PROCESSING = 1;
+    private static final int FAILURE = -1;
     private static final int IMAGE = 0;
 
-	private EditText pathText;
-	private TextView resultView;
-	private Button downloadButton;
-	private Button stopButton;
+    private EditText pathText;
+    private TextView resultView;
+    private Button downloadButton;
+    private Button stopButton;
     private TextView music_name;
-	private ProgressBar progressBar;
-	private Button playBtn;
-	private Player player;
-	private SeekBar musicProgress;
-	private Handler handler = new UIHandler();
+    private ProgressBar progressBar;
+    private ImageButton playBtn;
+    private ImageButton NextBtn;
+    //private ImageButton DislikeBtn;
+    private Player player;
+    private SeekBar musicProgress;
+    private Handler handler = new UIHandler();
     private NetworkImageView networkImageView;
+    private boolean musicPause = false;
 
     private final class UIHandler extends Handler {
-		public void handleMessage(Message msg) {
-			switch (msg.what) {
-			case PROCESSING:
-				progressBar.setProgress(msg.getData().getInt("size"));
-				float num = (float) progressBar.getProgress()
-						/ (float) progressBar.getMax();
-				int result = (int) (num * 100);
-				resultView.setText(result + "%");
-				if (progressBar.getProgress() == progressBar.getMax()) {
-					Toast.makeText(getApplicationContext(), R.string.success,
-							Toast.LENGTH_LONG).show();
-				}
-				break;
-			case FAILURE:
-				Toast.makeText(getApplicationContext(), R.string.error,
-						Toast.LENGTH_LONG).show();
-				break;
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case PROCESSING:
+                    progressBar.setProgress(msg.getData().getInt("size"));
+                    float num = (float) progressBar.getProgress()
+                            / (float) progressBar.getMax();
+                    int result = (int) (num * 100);
+                    resultView.setText(result + "%");
+                    if (progressBar.getProgress() == progressBar.getMax()) {
+                        Toast.makeText(getApplicationContext(), R.string.success,
+                                Toast.LENGTH_LONG).show();
+                    }
+                    break;
+                case FAILURE:
+                    Toast.makeText(getApplicationContext(), R.string.error,
+                            Toast.LENGTH_LONG).show();
+                    break;
 
                 case IMAGE:
                     Bundle bundle = msg.getData();
@@ -81,6 +86,7 @@ public class MainActivity extends Activity {
                         @Override
                         public void putBitmap(String url, Bitmap bitmap) {
                         }
+
                         @Override
                         public Bitmap getBitmap(String url) {
                             return null;
@@ -88,150 +94,174 @@ public class MainActivity extends Activity {
                     });
                     networkImageView.setImageUrl(cover, imageLoader);
                     break;
-			}
-		}
-	}
+            }
+        }
+    }
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		pathText = (EditText) findViewById(R.id.path);
-		resultView = (TextView) findViewById(R.id.resultView);
-		downloadButton = (Button) findViewById(R.id.downloadbutton);
-		stopButton = (Button) findViewById(R.id.stopbutton);
-		progressBar = (ProgressBar) findViewById(R.id.progressBar);
-		ButtonClickListener listener = new ButtonClickListener();
-		downloadButton.setOnClickListener(listener);
-		stopButton.setOnClickListener(listener);
-		playBtn = (Button) findViewById(R.id.btn_online_play);
-		playBtn.setOnClickListener(listener);
-		musicProgress = (SeekBar) findViewById(R.id.music_progress);
-		player = new Player(musicProgress);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        pathText = (EditText) findViewById(R.id.path);
+        resultView = (TextView) findViewById(R.id.resultView);
+        downloadButton = (Button) findViewById(R.id.downloadbutton);
+        stopButton = (Button) findViewById(R.id.stopbutton);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        ButtonClickListener listener = new ButtonClickListener();
+        downloadButton.setOnClickListener(listener);
+        stopButton.setOnClickListener(listener);
+        playBtn = (ImageButton) findViewById(R.id.btn_online_play);
+        NextBtn = (ImageButton) findViewById(R.id.btn_next_song);
+        //DislikeBtn = (ImageButton) findViewById(R.id.btn_play_music);
+        playBtn.setOnClickListener(listener);
+        NextBtn.setOnClickListener(listener);
+        //DislikeBtn.setOnClickListener(listener);
+        musicProgress = (SeekBar) findViewById(R.id.music_progress);
+        player = new Player(musicProgress);
         music_name = (TextView) findViewById(R.id.music_name);
-		musicProgress.setOnSeekBarChangeListener(new SeekBarChangeEvent());
+        musicProgress.setOnSeekBarChangeListener(new SeekBarChangeEvent());
         networkImageView = (NetworkImageView) findViewById(R.id.network_image_view);
     }
 
-	private final class ButtonClickListener implements View.OnClickListener {
-		@Override
-		public void onClick(View v) {
-			System.out.println("xxx");
-			switch (v.getId()) {
-			case R.id.downloadbutton:
-				String path = pathText.getText().toString();
-				String filename = path.substring(path.lastIndexOf('/') + 1);
+    private final class ButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            System.out.println("xxx");
+            switch (v.getId()) {
+                case R.id.downloadbutton:
+                    String path = pathText.getText().toString();
+                    String filename = path.substring(path.lastIndexOf('/') + 1);
+                    try {
+                        filename = URLEncoder.encode(filename, "UTF-8");
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
 
-				try {
-					filename = URLEncoder.encode(filename, "UTF-8");
-				} catch (UnsupportedEncodingException e) {
-					e.printStackTrace();
-				}
+                    path = path.substring(0, path.lastIndexOf("/") + 1) + filename;
+                    if (Environment.getExternalStorageState().equals(
+                            Environment.MEDIA_MOUNTED)) {
+                        File savDir = Environment.getExternalStorageDirectory();
+                        download(path, savDir);
+                    } else {
+                        Toast.makeText(getApplicationContext(),
+                                R.string.sdcarderror, Toast.LENGTH_LONG).show();
+                    }
+                    downloadButton.setEnabled(false);
+                    stopButton.setEnabled(true);
+                    break;
+                case R.id.stopbutton:
+                    exit();
+                    Toast.makeText(getApplicationContext(),
+                            "Now thread is Stopping!!", Toast.LENGTH_LONG).show();
+                    downloadButton.setEnabled(true);
+                    stopButton.setEnabled(false);
+                    break;
+                case R.id.btn_online_play:
+                    System.out.print(musicPause);
+                    if(player.mediaPlayer.isPlaying())
+                    {
+                        player.pause();
+                        musicPause = true;
+                        playBtn.setImageResource(R.drawable.player_pause);
+                    }
+                    else if(!player.mediaPlayer.isPlaying()){
+                        if(musicPause)
+                        {
+                            player.play();
+                            musicPause = false;
+                            playBtn.setImageResource(R.drawable.player_play);
+                        } else
+                        {
+                            playMusic();
+                        }
+                    }
+                    break;
 
-				path = path.substring(0, path.lastIndexOf("/") + 1) + filename;
-				if (Environment.getExternalStorageState().equals(
-						Environment.MEDIA_MOUNTED)) {
-					File savDir = Environment.getExternalStorageDirectory();
-					download(path, savDir);
-				} else {
-					Toast.makeText(getApplicationContext(),
-							R.string.sdcarderror, Toast.LENGTH_LONG).show();
-				}
-				downloadButton.setEnabled(false);
-				stopButton.setEnabled(true);
-				break;
-			case R.id.stopbutton:
-				exit();
-				Toast.makeText(getApplicationContext(),
-						"Now thread is Stopping!!", Toast.LENGTH_LONG).show();
-				downloadButton.setEnabled(true);
-				stopButton.setEnabled(false);
-				break;
-			case R.id.btn_online_play:
-                playMusic();
-			}
-		}
+                case R.id.btn_next_song:
+                    playMusic();
+                    break;
+            }
+        }
 
-		private DownloadTask task;
+        private DownloadTask task;
 
-		private void exit() {
-			if (task != null)
-				task.exit();
-		}
+        private void exit() {
+            if (task != null)
+                task.exit();
+        }
 
-		private void download(String path, File savDir) {
-			task = new DownloadTask(path, savDir);
-			new Thread(task).start();
-		}
+        private void download(String path, File savDir) {
+            task = new DownloadTask(path, savDir);
+            new Thread(task).start();
+        }
 
-		private final class DownloadTask implements Runnable {
-			private String path;
-			private File saveDir;
-			private FileDownloader loader;
+        private final class DownloadTask implements Runnable {
+            private String path;
+            private File saveDir;
+            private FileDownloader loader;
 
-			public DownloadTask(String path, File saveDir) {
-				this.path = path;
-				this.saveDir = saveDir;
-			}
+            public DownloadTask(String path, File saveDir) {
+                this.path = path;
+                this.saveDir = saveDir;
+            }
 
-			public void exit() {
-				if (loader != null)
-					loader.exit();
-			}
+            public void exit() {
+                if (loader != null)
+                    loader.exit();
+            }
 
-			DownloadProgressListener downloadProgressListener = new DownloadProgressListener() {
-				@Override
-				public void onDownloadSize(int size) {
-					Message msg = new Message();
-					msg.what = PROCESSING;
-					msg.getData().putInt("size", size);
-					handler.sendMessage(msg);
-				}
-			};
+            DownloadProgressListener downloadProgressListener = new DownloadProgressListener() {
+                @Override
+                public void onDownloadSize(int size) {
+                    Message msg = new Message();
+                    msg.what = PROCESSING;
+                    msg.getData().putInt("size", size);
+                    handler.sendMessage(msg);
+                }
+            };
 
-			public void run() {
-				try {
-					loader = new FileDownloader(getApplicationContext(), path, saveDir, 3);
-					progressBar.setMax(loader.getFileSize());
-					loader.download(downloadProgressListener);
-				} catch (Exception e) {
-					e.printStackTrace();
-					handler.sendMessage(handler.obtainMessage(FAILURE));
-				}
-			}
-		}
+            public void run() {
+                try {
+                    loader = new FileDownloader(getApplicationContext(), path, saveDir, 3);
+                    progressBar.setMax(loader.getFileSize());
+                    loader.download(downloadProgressListener);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    handler.sendMessage(handler.obtainMessage(FAILURE));
+                }
+            }
+        }
     }
 
-	class SeekBarChangeEvent implements OnSeekBarChangeListener {
-		int progress;
+    class SeekBarChangeEvent implements OnSeekBarChangeListener {
+        int progress;
 
-		@Override
-		public void onProgressChanged(SeekBar seekBar, int progress,
-				boolean fromUser) {
-			this.progress = progress * player.mediaPlayer.getDuration()
-					/ seekBar.getMax();
-            if(progress == 99)
-            {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress,
+                                      boolean fromUser) {
+            this.progress = progress * player.mediaPlayer.getDuration()
+                    / seekBar.getMax();
+            if (progress == 99) {
                 playMusic();
             }
-		}
+        }
 
-		@Override
-		public void onStartTrackingTouch(SeekBar seekBar) {
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
 
-		}
+        }
 
-		@Override
-		public void onStopTrackingTouch(SeekBar seekBar) {
-			player.mediaPlayer.seekTo(progress);
-		}
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+            player.mediaPlayer.seekTo(progress);
+        }
 
-	}
+    }
 
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		if (player != null) {
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (player != null) {
             player.stop();
             player = null;
         }
@@ -240,7 +270,6 @@ public class MainActivity extends Activity {
     private JSONObject getSong() {
         HttpClient client = new DefaultHttpClient();
         StringBuilder builder = new StringBuilder();
-        String musicUrl;
         HttpGet get = new HttpGet("http://hr.hgdonline.net/music/player.php");
         try {
             HttpResponse response = client.execute(get);
@@ -256,8 +285,7 @@ public class MainActivity extends Activity {
         return null;
     }
 
-    public void playMusic()
-    {
+    public void playMusic() {
         new Thread(new Runnable() {
             @Override
             public void run() {
